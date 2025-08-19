22a04f6f950ea52b6779a07626e324a60c378ba8
NitroQuake <loganteh05@gmail.com>
2025-08-19 16:23:50 -0700

    Implement property listing and details with Appwrite integration
    Refactored explore, index, and property detail screens to fetch and display property data from Appwrite. Added components for comments and no results, updated Cards and Filters to use dynamic data, and corrected category values. Introduced Appwrite data fetching utilities and a seed script for populating the database.

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,55 +1,107 @@
-import {Text, View, Image, TouchableOpacity} from "react-native";
-import {Link} from "expo-router";
+import {Text, View, Image, TouchableOpacity, FlatList, ActivityIndicator} from "react-native";
+import {router, useLocalSearchParams} from "expo-router";
 import {SafeAreaView} from "react-native-safe-area-context";
 
-import images from "@/constants/images";
 import icons from "@/constants/icons";
 import Search from "@/components/Search";
 import {Card, FeaturedCard} from "@/components/Cards";
 import Filters from "@/components/Filters";
+import {useGlobalContext} from "@/lib/global-provider";
+import {getLatestProperties, getProperties} from "@/lib/appwrite";
+import {useAppwrite} from "@/lib/useAppwrite";
+import {useEffect} from "react";
+import NoResults from "@/components/NoResults";
 
 export default function Index() {
-  return (
+    const {user} = useGlobalContext()
+    const params = useLocalSearchParams<{query?: string; filter?: string;}>();
+
+    // Fetch the latest properties using the Appwrite function
+    const {data: latestProperties, loading: latestPropertiesLoading} = useAppwrite({
+        fn: getLatestProperties
+    });
+
+    // Fetch properties based on the filter and query parameters
+    const {data: properties, loading, refetch} = useAppwrite({
+        fn: getProperties,
+        params: {
+            filter: params.filter!,
+            query: params.query!,
+            limit: 6,
+        },
+        skip: true,
+    });
+
+    const handleCardPress = (id: string) => router.push(`/properties/${id}`);
+
+    // Refetch properties when the component mounts or when the filter/query parameters change
+    useEffect(() => {
+        refetch({
+            filter: params.filter!,
+            query: params.query!,
+            limit: 6,
+        });
+    }, [params.filter, params.query]);
+
+    return (
     <SafeAreaView className={"bg-white h-full"}>
-        <View className={"px-5"}>
-            <View className={"flex flex-row items-center justify-between mt-5"}>
-                <View className={"flex flex-row items-center"}>
-                    <Image source={images.avatar} className={"size-12 rounded-full"} />
-                    <View className={"flex flex-col items-start ml-2 justify-center"}>
-                        <Text className={"text-xs font-rubik text-black-100"}>Good Morning</Text>
-                        <Text className={"text-base font-rubik-medium text-black-300"}>Adrian</Text>
+        <FlatList
+            data={properties}
+            renderItem={({item}) => <Card item={item} onPress={() => handleCardPress(item.$id)}/>}
+            keyExtractor={(item) => item.$id}
+            numColumns={2} contentContainerClassName={"pb-32"}
+            columnWrapperClassName={"flex gap-5 px-5"}
+            showsVerticalScrollIndicator={false}
+            ListEmptyComponent={
+                loading ? (
+                    <ActivityIndicator size="large" className={"text-primary-300 mt-5"}/>
+                ) : <NoResults />
+            }
+            ListHeaderComponent={
+                <View className={"px-5"}>
+                    <View className={"flex flex-row items-center justify-between mt-5"}>
+                        <View className={"flex flex-row items-center"}>
+                            <Image source={{uri: user?.avatar}} className={"size-12 rounded-full"} />
+                            <View className={"flex flex-col items-start ml-2 justify-center"}>
+                                <Text className={"text-xs font-rubik text-black-100"}>Good Morning</Text>
+                                <Text className={"text-base font-rubik-medium text-black-300"}>{user?.name}</Text>
+                            </View>
+                        </View>
+                        <Image source={icons.bell} className={"size-6"} />
                     </View>
-                </View>
-                <Image source={icons.bell} className={"size-6"} />
-            </View>
-            <Search/>
-            <View className={"my-5"}>
-                <View className={"flex flex-row items-center justify-between"}>
-                    <Text className={"text-xl font-rubik-bold text-black-300"}>Featured</Text>
-                    <TouchableOpacity>
-                        <Text className={"text-base font-rubik-bold text-primary-300"}>See All</Text>
-                    </TouchableOpacity>
-                </View>
+                    <Search/>
+                    <View className={"my-5"}>
+                        <View className={"flex flex-row items-center justify-between"}>
+                            <Text className={"text-xl font-rubik-bold text-black-300"}>Featured</Text>
+                            <TouchableOpacity>
+                                <Text className={"text-base font-rubik-bold text-primary-300"}>See All</Text>
+                            </TouchableOpacity>
+                        </View>
 
-                <View className={"flex flex-row gap-5 mt-5"}>
-                    <FeaturedCard/>
-                    <FeaturedCard/>
+                        {latestPropertiesLoading ?
+                            <ActivityIndicator size="large" className={"text-primary-300"}/>
+                            : !latestProperties || latestProperties.length === 0 ? <NoResults /> : (
+                            <FlatList
+                                data={latestProperties}
+                                renderItem={({item}) => <FeaturedCard item={item} onPress={() => handleCardPress(item.$id)}/>}
+                                keyExtractor={(item) => item.$id}
+                                horizontal
+                                bounces={false}
+                                showsHorizontalScrollIndicator={false}
+                                contentContainerClassName={"flex gap-5 mt-5"}
+                            />)
+                        }
+                    </View>
+                    <View className={"flex flex-row items-center justify-between"}>
+                        <Text className={"text-xl font-rubik-bold text-black-300"}>Our Recommendation</Text>
+                        <TouchableOpacity>
+                            <Text className={"text-base font-rubik-bold text-primary-300"}>See All</Text>
+                        </TouchableOpacity>
+                    </View>
+                    <Filters/>
                 </View>
-            </View>
-            <View className={"flex flex-row items-center justify-between"}>
-                <Text className={"text-xl font-rubik-bold text-black-300"}>Our Recommendation</Text>
-                <TouchableOpacity>
-                    <Text className={"text-base font-rubik-bold text-primary-300"}>See All</Text>
-                </TouchableOpacity>
-            </View>
-
-            <Filters/>
-
-            <View className={"flex flex-row gap-5 mt-5"}>
-                <Card/>
-                <Card/>
-            </View>
-        </View>
+            }
+        />
     </SafeAreaView>
   );
 }
